9b5c8644c1700ef239a60bbe5f28a8529f17c7db
kinjal-savitara <kinjal@savitarainfotel.com>
2025-09-03 16:43:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return ( 
    <div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: 9999 }}>
 
-  <iframe src="https://biganto.com/tour/46401/?iframe=1" width="100%" height="600" frameborder="0" scrolling="0" seamless="seamless" allowfullscreen="allowfullscreen"></iframe>
+  <iframe src="https://biganto.com/tour/46401/?iframe=1" width="100%" height="100%" frameborder="0" scrolling="0" seamless="seamless" allowfullscreen="allowfullscreen"></iframe>
   
     </div>
   )
